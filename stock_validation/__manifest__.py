7a4461e88a7cb07ebff151ddc5521b20cb8888e7
GabrielSnader <gabriel.lopez@quadit.mx>
2019-08-02 12:23:54 -0500

    stock_validation

---
+++ stock_validation/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Validation Tranfers',
+    'version': '1.1',
+    'summary': '',
+    'description': "",
+    'website': '',
+    'depends': ['stock'],
+    'category': 'Inventary',
+    'sequence': 13,
+    'demo': [
+    ],
+    'data': [
+        'stock_views.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
